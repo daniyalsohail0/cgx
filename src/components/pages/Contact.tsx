40a2de37fa95002f5dce9555988f06ae92ed4727
Daniyal Sohail <daniyalsohail0@gmail.com>
2024-08-14 12:10:11 +0100

    edit

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ContactForm from "./ContactForm";
 
-const Contact = () => {
+const Contact: React.FC = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center md:w-3/4 w-11/12 py-16">
       <div className="md:flex flex-col gap-10 md:w-1/2 w-full hidden">
         <h1 className="text-4xl font-bold tracking-wide">Contact CGX</h1>
         <p className="text-gray-700 tracking-wide">
           We appreciate your interest in Cyber Genesis X. Whether you have a
-          specific project in mind or would like to lerarn more about what we,
-          drop us a line below.
+          specific project in mind or would like to learn more about what we
+          do, drop us a line below.
         </p>
         <p>
           <span className="font-semibold">Call us:</span>{" "}
@@ -26,7 +26,7 @@ const Contact = () => {
         </p>
         <p>
           <span className="font-semibold">Cyber Genesis X</span> <br /> 34
-          Azelea Close <br />
+          Azalea Close <br />
           Barking
           <br />
           London
@@ -35,11 +35,9 @@ const Contact = () => {
         </p>
         <div>
           <em>What happens after sending a message?</em>
-          <ol>
-            <li> • We'll email over an introductory pack straight away,</li>
-            <li>
-              • One of our representatives will be in touch within 24 hours.
-            </li>
+          <ol className="list-decimal pl-6">
+            <li>We'll email over an introductory pack straight away.</li>
+            <li>One of our representatives will be in touch within 24 hours.</li>
           </ol>
         </div>
       </div>
